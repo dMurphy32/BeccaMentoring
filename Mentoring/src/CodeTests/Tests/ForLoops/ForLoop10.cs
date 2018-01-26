0af296387ef720951a300b401db33a95ebbe58cc
Danny.Murphy <Danny.Murphy@perfectchannel.com>
2018-01-26 15:23:52 +0000

    Added tests

---
+++ Mentoring/src/CodeTests/Tests/ForLoops/ForLoop10.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CodeTests.Models;
+
+namespace CodeTests.Tests.ForLoops
+{
+    public class ForLoop10 : ICodeTest
+    {
+        public void Run()
+        {
+            var data = Initialise();
+            // TODO Sum up all the numbers across all the arrays
+        }
+
+        private IList<NumberArray> Initialise()
+        {
+            var result = new List<NumberArray>();
+
+            result.Add(new NumberArray //object 1
+            {
+                Numbers = new[] { 1, 2, 3, 4, 5 }
+            });
+            result.Add(new NumberArray // object 2
+            {
+                Numbers = new[] { 6, 7, 8, 9, 10 }
+            });
+            result.Add(new NumberArray //object 3
+            {
+                Numbers = new[] { 11, 12, 13, 14, 15 }
+            });
+
+            return result;
+        }
+    }
+}
---
+++ Mentoring/src/CodeTests/Tests/WhileLoops/WhileLoop4.cs
@@ -0,0 +1,11 @@
+﻿namespace CodeTests.Tests.WhileLoops
+{
+    public class WhileLoop4 : ICodeTest
+    {
+        public void Run()
+        {
+            var array = new[] { 1, 2, 3, 4, 5, 6 };
+            //TODO: Ask a user for an input and compare it against an expected value, if not ask again, ask a maximum of 5 times before quitting
+        }
+    }
+}
