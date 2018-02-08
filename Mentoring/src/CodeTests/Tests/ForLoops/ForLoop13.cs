edf2435f1e4da4b3414eaa7d0fbd626dfc73c3c0
Danny.Murphy <Danny.Murphy@perfectchannel.com>
2018-02-08 12:31:17 +0000

    Added initi with neg numbers for new tests

--- Mentoring/src/CodeTests/Tests/ForLoops/ForLoop13.cs
+++ Mentoring/src/CodeTests/Tests/ForLoops/ForLoop13.cs
@@ -14,6 +14,7 @@ namespace CodeTests.Tests.ForLoops
             var data = Initialise();
 
             //TODO: Find the array with the smallest sum of numbers
+            //TODO: Initialise data with InitialiseWithNegativeNumbers and assert it works
         }
 
         private IList<NumberArray> Initialise()
@@ -31,6 +32,26 @@ namespace CodeTests.Tests.ForLoops
             result.Add(new NumberArray //object 3
             {
                 Numbers = new[] { 11, 12, 13, 14, 15 }
+            });
+
+            return result;
+        }
+
+        private IList<NumberArray> InitialiseWithNegativeNumbers()
+        {
+            var result = new List<NumberArray>();
+
+            result.Add(new NumberArray //object 1
+            {
+                Numbers = new[] { -1, -2, -3, -4, -5 }
+            });
+            result.Add(new NumberArray // object 2
+            {
+                Numbers = new[] { -6, -7, -8, -9, -10 }
+            });
+            result.Add(new NumberArray //object 3
+            {
+                Numbers = new[] { -11, -12, -13, -14, -15 }
             });
 
             return result;
