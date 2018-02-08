d8c40d323d4ee54bd5772a990d972ec751fab4d0
Danny.Murphy <Danny.Murphy@perfectchannel.com>
2018-02-08 12:28:33 +0000

    Added 3 more tests

---
+++ Mentoring/src/CodeTests/Tests/ForLoops/ForLoop11.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeTests.Tests.ForLoops
+{
+    public class ForLoop11 : ICodeTest
+    {
+        public void Run()
+        {
+            var array = new[] { 7, 45, 5, 98, 4, 90 };
+            //TODO: Loop through array and exit it if the loop's index equals the arrays value at that  value (@ 4)
+        }
+    }
+}
---
+++ Mentoring/src/CodeTests/Tests/ForLoops/ForLoop13.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CodeTests.Models;
+
+namespace CodeTests.Tests.ForLoops
+{
+    public class ForLoop13 : ICodeTest
+    {
+        public void Run()
+        {
+            var data = Initialise();
+
+            //TODO: Find the array with the smallest sum of numbers
+        }
+
+        private IList<NumberArray> Initialise()
+        {
+            var result = new List<NumberArray>();
+
+            result.Add(new NumberArray //object 1
+            {
+                Numbers = new[] { 1, 2, 3, 4, 5 }
+            });
+            result.Add(new NumberArray // object 2
+            {
+                Numbers = new[] { 6, 7, 8, 9, 10 }
+            });
+            result.Add(new NumberArray //object 3
+            {
+                Numbers = new[] { 11, 12, 13, 14, 15 }
+            });
+
+            return result;
+        }
+    }
+}
