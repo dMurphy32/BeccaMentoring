a0585afabd70782ef7387bfd2e50de6cd0b4eded
PC\Rebecca.Gatt <rebecca.gatt@perfectchannel.com>
2018-02-16 16:36:55 +0000

    ForLoop5 unit test

---
+++ Mentoring/CodeTest.UnitTests/ForLoops/EverySecondNumberTestFixture.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CodeTests.Tests.ForLoops;
+using NUnit.Framework;
+
+namespace CodeTest.UnitTests.ForLoops
+{
+    [TestFixture]
+    public class EverySecondNumberTestFixture
+    {
+        [Test]
+        public void Run_Invoke_EverySecondNumber()
+        {
+            //Arrange
+            var input = new[] {1, 2, 3, 4, 5, 6};
+            var expected = new List<int>
+            {
+                0,
+                2,
+                4,
+                6
+            };
+            var subject = new EverySecondNumber();
+
+            //Act
+            var result = subject.Run(input);
+
+            //Assert
+            for (int i = 0; i < expected.Count; i= i +2)
+            {
+                Assert.AreEqual(expected[i], result[i]);
+            }
+        }
+    }
+}
--- Mentoring/src/CodeTests/Tests/ForLoops/ForLoop5.cs
+++ Mentoring/src/CodeTests/Tests/ForLoops/ForLoop5.cs
@@ -6,22 +6,27 @@ using System.Threading.Tasks;
 
 namespace CodeTests.Tests.ForLoops
 {
-    public class ForLoop5 : ICodeTest
+    public class EverySecondNumber
     {
-        public void Run()
+        public List<int> Run(int[] input)
         {
-            var array = new[] { 1, 2, 3, 4, 5, 6 };
+            //var array = new[] { 1, 2, 3, 4, 5, 6 };
             // TODO Print every second number in the array without an if statement, do the same in reverse 
 
-            for (int i = 0; i <= array.Length; i = i + 2)
+            var result = new List<int>();
+            for (int i = 0; i < input.Length; i = i + 2)
             {
                 Console.WriteLine(i);
+                result.Add(i);
             }
 
-            for (int i = array.Length; i >= 0; i = i - 2)
-            {
-                Console.WriteLine(i);
-            }
+            //for (int i = input.Length; i >= 0; i = i - 2)
+            //{
+            //    Console.WriteLine(i);
+            //    result.Add(i);
+            //}
+
+            return result;
         }
     }
 }
