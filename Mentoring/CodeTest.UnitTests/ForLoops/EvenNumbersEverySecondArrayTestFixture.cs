d8418b78c83921c9a931bb05f8fec9c99b4c879f
PC\Rebecca.Gatt <rebecca.gatt@perfectchannel.com>
2018-02-19 18:14:38 +0000

    ForLoop9 unit test

---
+++ Mentoring/CodeTest.UnitTests/ForLoops/EvenNumbersEverySecondArrayTestFixture.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using CodeTests.Models;
+using CodeTests.Tests.ForLoops;
+using NUnit.Framework;
+
+namespace CodeTest.UnitTests.ForLoops
+{
+    [TestFixture]
+    public class EvenNumbersEverySecondArrayTestFixture
+    {
+        private IList<NumberArray> Initialise()
+        {
+            var result = new List<NumberArray>();
+            result.Add(new NumberArray
+            {
+                Numbers = new List<int> { 1, 2, 3, 4, 5 }
+            });
+            result.Add(new NumberArray
+            {
+                Numbers = new List<int> { 6, 7, 8, 9, 10 }
+            });
+            result.Add(new NumberArray
+            {
+                Numbers = new List<int> { 11, 12, 13, 14, 15 }
+            });
+            result.Add(new NumberArray
+            {
+                Numbers = new List<int> { 724, 53, 44, 16, 15 }
+            });
+
+            return result;
+        }
+
+        [Test]
+        public void Run_Invoke_EvenNumbersEverySecondArray()
+        {
+            //Arrange
+            var input = Initialise();
+            var expected = new List<int>
+            {
+                6,
+                8,
+                10,
+                724,
+                44,
+                16
+            };
+            var subject = new EvenNumbersEverySecondArray();
+
+            //Act
+            var result = subject.Run(input);
+
+            //Assert
+            for (int i = 1; i < input.Count; i=i+2)
+            {
+                for (int j = 0; j < input[i].Numbers.Count; j++)
+                {
+                    var numSet = input[i].Numbers[j];
+                    if (numSet%2 ==0)
+                    {
+                        Assert.AreEqual(expected, result);
+                    }
+                }
+            }
+        }
+    }
+}
--- Mentoring/src/CodeTests/Tests/ForLoops/ForLoop9.cs
+++ Mentoring/src/CodeTests/Tests/ForLoops/ForLoop9.cs
@@ -7,43 +7,25 @@ using CodeTests.Models;
 
 namespace CodeTests.Tests.ForLoops
 {
-    public class ForLoop9 : ICodeTest
+    public class EvenNumbersEverySecondArray
     {
-        public void Run()
+        public List<int> Run(IList<NumberArray> input)
         {
-            var data = Initialise();
+            var result = new List<int>();
             // TODO Print the even numbers from every second array
 
-            for (int i = 1; i < data.Count; i = i+2)
+            for (int i = 1; i < input.Count; i = i+2)
             {
-                for (int j = 0; j < data[i].Numbers.Length; j++)
+                for (int j = 0; j < input[i].Numbers.Count; j++)
                 {
-                    var numSet = data[i].Numbers[j];
+                    var numSet = input[i].Numbers[j];
                     if (numSet%2 == 0)
                     {
                         Console.WriteLine(numSet);
+                        result.Add(numSet);
                     }
                 }
             }
-        }
-
-        private IList<NumberArray> Initialise()
-        {
-            var result = new List<NumberArray>();
-
-            result.Add(new NumberArray //object 1
-            {
-                Numbers = new[] { 1, 2, 3, 4, 5 }
-            });
-            result.Add(new NumberArray // object 2
-            {
-                Numbers = new[] { 6, 7, 8, 9, 10 }
-            });
-            result.Add(new NumberArray //object 3
-            {
-                Numbers = new[] { 11, 12, 13, 14, 15 }
-            });
-
             return result;
         }
     }
