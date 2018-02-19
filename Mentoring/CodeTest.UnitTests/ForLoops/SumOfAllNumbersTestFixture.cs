8f5e78ead7399b598227a170f4c41844a8187844
PC\Rebecca.Gatt <rebecca.gatt@perfectchannel.com>
2018-02-19 18:35:24 +0000

    ForLoop10 unit test

---
+++ Mentoring/CodeTest.UnitTests/ForLoops/SumOfAllNumbersTestFixture.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CodeTests.Models;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+using CodeTests.Tests.ForLoops;
+
+namespace CodeTest.UnitTests.ForLoops
+{
+    [TestFixture]
+    public class SumOfAllNumbersTestFixture
+    {
+        private IList<NumberArray> Initialise()
+        {
+            var result = new List<NumberArray>();
+
+            result.Add(new NumberArray
+            {
+                Numbers = new List<int> { 1, 2, 3, 4, 5 }
+            });
+            result.Add(new NumberArray
+            {
+                Numbers = new List<int> { 6, 7, 8, 9, 10 }
+            });
+            result.Add(new NumberArray
+            {
+                Numbers = new List<int> { 11, 12, 13, 14, 456 }
+            });
+
+            return result;
+        }
+
+        [Test]
+        public void Run_Invoke_SumOfAllNumbersTestFixture()
+        {
+            //Arrange
+            var input = Initialise();
+            var expected = new List<int>
+            {
+                561
+            };
+            var subject = new SumOfAllNumbers();
+
+            //Act
+            var result = subject.Run(input);
+
+            //Assert
+            var sum = 0;
+            var total = sum + sum + sum;
+            for (int i = 0; i < input.Count; i++)
+            {
+                sum = 0;
+                for (int j = 0; j < input[i].Numbers.Count; j++)
+                {
+                    sum += input[i].Numbers[j];
+                }
+
+                total += sum;
+            }
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
+
+
+
--- Mentoring/src/CodeTests/Tests/ForLoops/ForLoop10.cs
+++ Mentoring/src/CodeTests/Tests/ForLoops/ForLoop10.cs
@@ -1,54 +1,31 @@
-﻿using System;
+﻿using CodeTests.Models;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Diagnostics.CodeAnalysis;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml.Schema;
-using CodeTests.Models;
 
 namespace CodeTests.Tests.ForLoops
 {
-    public class ForLoop10 : ICodeTest
+    public class SumOfAllNumbers
     {
-        public void Run()
+        public List<int> Run(IList<NumberArray> input)
         {
-            var data = Initialise();
+            var result = new List<int>();
             // TODO Sum up all the numbers across all the arrays
 
             var sum = 0;
             var total = sum + sum + sum;
-            for (int i = 0; i < data.Count; i++)
+            for (int i = 0; i < input.Count; i++)
             {
                 sum = 0;
-                for (int j = 0; j < data[i].Numbers.Length; j++)
+                for (int j = 0; j < input[i].Numbers.Count; j++)
                 {
-                    sum += data[i].Numbers[j];
+                    sum += input[i].Numbers[j];
                     //total += i;
                 }
                 Console.WriteLine($"The sum of object {i} = {sum}");
                 total += sum;
             }
             Console.WriteLine($"total is {total}");
-        }
-
-        private IList<NumberArray> Initialise()
-        {
-            var result = new List<NumberArray>();
-
-            result.Add(new NumberArray //object 1
-            {
-                Numbers = new[] { 1, 2, 3, 4, 5 }
-            });
-            result.Add(new NumberArray // object 2
-            {
-                Numbers = new[] { 6, 7, 8, 9, 10 }
-            });
-            result.Add(new NumberArray //object 3
-            {
-                Numbers = new[] { 11, 12, 13, 14, 15 }
-            });
+            result.Add(total);
 
             return result;
         }
