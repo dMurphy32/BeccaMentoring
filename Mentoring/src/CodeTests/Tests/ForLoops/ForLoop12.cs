bfc597394acc846960287e9e86f54c3549821418
PC\Rebecca.Gatt <rebecca.gatt@perfectchannel.com>
2018-02-22 16:47:12 +0000

    ForLoop12 solution

--- Mentoring/src/CodeTests/Tests/ForLoops/ForLoop12.cs
+++ Mentoring/src/CodeTests/Tests/ForLoops/ForLoop12.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Schema;
 using CodeTests.Models;
 
 namespace CodeTests.Tests.ForLoops
@@ -12,26 +13,62 @@ namespace CodeTests.Tests.ForLoops
         public void Run()
         {
             var data = Initialise();
+            var dataNeg = InitialiseWithNegativeNumbers();
 
             //TODO: Find the array with the largest sum of numbers
             //TODO: Initialise data with InitialiseWithNegativeNumbers and assert it works
+
+            
+            var total = 0;
+            for (int i = 0; i < data.Count; i++)
+            {
+                var sum = 0;
+                for (int j = 0; j < data[i].Numbers.Count; j++)
+                {
+                    sum += data[i].Numbers[j];
+                }
+                total += sum;
+            }
+            Console.WriteLine(total);
+        
+
+            var totalNeg = 0;
+                for (int i = 0; i<dataNeg.Count; i++)
+            {
+                var sum = 0;
+                for (int j = 0; j<dataNeg[i].Numbers.Count; j++)
+                {
+                    sum += dataNeg[i].Numbers[j];
+                }
+                totalNeg += sum;
+            }
+            Console.WriteLine(totalNeg);
+
+            if (total > totalNeg)
+            {
+                Console.WriteLine($"Largest sum of numbers is first array with a total of {total}");
+            }
+            else
+            {
+                Console.WriteLine($"Largest sum of numbers is second array with a total of {totalNeg}");
+            }
         }
 
-        private IList<NumberArray> Initialise()
+private IList<NumberArray> Initialise()
         {
             var result = new List<NumberArray>();
 
             result.Add(new NumberArray //object 1
             {
-                Numbers = new[] { 1, 2, 3, 4, 5 }
+                Numbers = new List<int> { 1, 2, 3, 4, 5 }
             });
             result.Add(new NumberArray // object 2
             {
-                Numbers = new[] { 6, 7, 8, 9, 10 }
+                Numbers = new List<int> { 6, 7, 8, 9, 10 }
             });
             result.Add(new NumberArray //object 3
             {
-                Numbers = new[] { 11, 12, 13, 14, 15 }
+                Numbers = new List<int> { 11, 12, 13, 14, 15 }
             });
 
             return result;
@@ -43,15 +80,15 @@ namespace CodeTests.Tests.ForLoops
 
             result.Add(new NumberArray //object 1
             {
-                Numbers = new[] { -1, -2, -3, -4, -5 }
+                Numbers = new List<int> { -1, -2, -3, -4, -5 }
             });
             result.Add(new NumberArray // object 2
             {
-                Numbers = new[] { -6, -7, -8, -9, -10 }
+                Numbers = new List<int> { -6, -7, -8, -9, -10 }
             });
             result.Add(new NumberArray //object 3
             {
-                Numbers = new[] { -11, -12, -13, -14, -15 }
+                Numbers = new List<int> { -11, -12, -13, -14, -15 }
             });
 
             return result;
